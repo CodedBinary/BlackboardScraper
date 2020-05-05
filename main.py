4407966ff8af62bdd61108bb77b976215c85d249
tomdx <thomasle0@outlook.com>
2020-05-05 20:45:34 +1000

    Fix flake8 errors
    Add get_credentials()

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
 import sys
 import os
 import requests
+import getpass
 from bs4 import BeautifulSoup
 
 
-def authenticate(username,password):
+def authenticate(username, password):
     authurl = "https://auth.uq.edu.au"
 
     redirect = requests.get(authurl)
@@ -14,21 +16,29 @@ def authenticate(username,password):
     payload = {
         'username': username,
         'password': password,
-        }
+    }
 
     session = requests.Session()
     login = session.post(loginurl, data=payload)
     print(login.url)
 
-    #request = session.get("https://learn.uq.edu.au/webapps/blackboard/execute/announcement?method=search&context=course_entry&course_id=_129055_1&handle=announcements_entry&mode=view")
-    #soup = BeautifulSoup(request.text, "html.parser")
-    #print(soup)
+    # request = session.get("https://learn.uq.edu.au/webapps/blackboard/execute/announcement?method=search&context=course_entry&course_id=_129055_1&handle=announcements_entry&mode=view")
+    # soup = BeautifulSoup(request.text, "html.parser")
+    # print(soup)
+
+
+def get_credentials():
+    username = input("Username: ")
+    password = getpass.getpass("Password: ")
+    return (username, password)
+
 
 def main(argv):
+    authenticate(*get_credentials())
     pass
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main(sys.argv)
 
 
